f32cd08491a90a7731766e759aa72202c897ed8a
atg-abhijay <atg.abhijay@gmail.com>
2018-11-21 03:14:05 -0500

    figured out the Priority Queue reordering problem and added comments throughout

--- Solution.java
+++ Solution.java
@@ -7,12 +7,31 @@ import java.util.*;
 class Solution {
     public int[] solution(int D, int[] A) {
         // write your code in Java SE 8
+
+        /**
+         * better to use a large constant instead
+         * of max value of int so that we can avoid
+         * overflow.
+         */
         final int largeValue = (int) Math.pow(10, 8);
         int numVertices = A.length;
         Vertex[] vertices = new Vertex[numVertices];
+        /**
+         * the predecessors obtained along the shortest
+         * path need not be the same as the parents
+         * mentioned in the given array A
+         */
         Vertex[] predecessors = new Vertex[numVertices];
+        /**
+         * some initializations
+         */
         init(largeValue, vertices, predecessors, numVertices, 0);
 
+        /**
+         * priority queue that will hold the vertices. they will
+         * be ordered in increasing order of shortest distances
+         * from the source vertex
+         */
         PriorityQueue<Vertex> pq = new PriorityQueue<Vertex>(new Comparator<Vertex>() {
             @Override
             public int compare(Vertex v1, Vertex v2) {
@@ -24,10 +43,18 @@ class Solution {
             pq.add(vertices[i]);
         }
 
+        /**
+         * adding edges to the vertices
+         * according to the parents of
+         * the given vertices
+         */
         for(int i = 0; i < numVertices; i++) {
             int parent = A[i];
             if(parent != -1) {
                 Vertex startVertex = vertices[parent];
+                /**
+                 * all the edges have weight 1
+                 */
                 startVertex.addEdge(new Edge(vertices[i], 1));
             }
         }
@@ -37,45 +64,105 @@ class Solution {
              * get the vertex closest
              * to the source vertex.
              */
+            /**
+             * ****************
+             * IMPORTANT
+             * ****************
+             * should not have pq.poll() over here.
+             * if we do that, we remove the known
+             * closest vertex and then the pq DOES
+             * reorder its elements as it does upon
+             * removal of an element, but then, when
+             * we go into the relaxEdge method and we
+             * update a vertex's shortest distance, that
+             * does NOT end up reordering the pq.
+             *
+             * this happens because a change made to the
+             * vertex object does not automatically trigger
+             * the pq to reorder itself. so, we should only
+             * peek at the top of the pq for now. update the
+             * relaxed edge's end vertex's shortest distance.
+             */
             Vertex u = pq.peek();
             for(Edge e: u.edges) {
-                relaxEdge(pq, u, e, predecessors);
+                relaxEdge(u, e, predecessors);
             }
+            /**
+             * after we are done relaxing the edges originating
+             * from the current closest vertex, the vertices which
+             * were connected to it have had their distances updated
+             * (or not). we remove the current closest vertex HERE
+             * (instead of before the loop) and NOW the pq's reordering
+             * kicks in since we have used a method of the pq (to remove
+             * the element at the top) and so we get the next closest vertex
+             * to the source, taking into account the updates that were
+             * made upon relaxation above.
+             */
             pq.poll();
         }
 
         int[] ancestorAtDistanceD = new int[numVertices];
         for(int i = 0; i < numVertices; i++) {
+            /**
+             * for each of the vertices:
+             * count keeps track of performing
+             * D number of iterations.
+             */
             int count = 0;
             int currentNode = i;
             while(count < D) {
-                Vertex parent = predecessors[currentNode];
-                if(parent == null) {
+                /**
+                 * get the ith node's ancestor along
+                 * the shortest path from the source vertex.
+                 * if the ancestor is -1, break and set the
+                 * ith node's ancestor at distance D to -1
+                 * else continue until we find the ancestor
+                 * at distance D.
+                 */
+                Vertex ancestor = predecessors[currentNode];
+                if(ancestor == null) {
                     currentNode = -1;
                     break;
                 }
-                currentNode = parent.number;
+                currentNode = ancestor.number;
                 count++;
             }
             ancestorAtDistanceD[i] = currentNode;
         }
 
-        printArray(ancestorAtDistanceD);
         return ancestorAtDistanceD;
 
     }
 
-    public void relaxEdge(PriorityQueue<Vertex> pq, Vertex startVertex, Edge e, Vertex[] predecessors) {
+
+    /**
+     * if there is a shorter way to get to the endvertex
+     * of the current edge, then we update its shortest
+     * distance from the source vertex and we update its
+     * predecessor to the start vertex of the given edge
+     */
+    public void relaxEdge(Vertex startVertex, Edge e, Vertex[] predecessors) {
         Vertex v = e.endVertex;
         if(v.shortestDistance > startVertex.shortestDistance + e.weight) {
-            // pq.remove(v);
+            /**
+             * updating the vertex's distance does not
+             * update the priority queue. it just changes
+             * the value for the Vertex object.
+             */
             v.shortestDistance = startVertex.shortestDistance + e.weight;
             System.out.println(startVertex.number + ", " + v.number + ", " + v.shortestDistance);
             predecessors[v.number] = startVertex;
-            // pq.add(v);
         }
     }
 
+
+    /**
+     * initialize the vertices as having shortest
+     * distance from the source equal to the large
+     * constant and set the predecessor of each
+     * of the vertices to null. the shortest distance
+     * of the source from the source is zero
+     */
     public void init(int largeValue, Vertex[] vertices, Vertex[] predecessors, int n, int sourceVertex) {
         for(int i = 0; i < n; i++) {
             vertices[i] = new Vertex(i, largeValue);
@@ -83,6 +170,7 @@ class Solution {
         }
         vertices[sourceVertex].shortestDistance = 0;
     }
+
 
     class Vertex {
         public int number;
@@ -99,6 +187,7 @@ class Solution {
         }
     }
 
+
     class Edge {
         public Vertex endVertex;
         public int weight;
@@ -107,13 +196,6 @@ class Solution {
             this.endVertex = v;
             this.weight = w;
         }
-    }
-
-    public void printArray(int[] arr) {
-        for(int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
     }
 
 }
